d29a9864f8c2f9bcc0ea1da7834f190ce3a2c8bc
Rafif <rafifali26@gmail.com>
2024-04-23 19:11:36 +0700

    Database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',
-        'password'     => 'root',
+        'password'     => '',
         'database'     => 'web_penjadwalan',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
